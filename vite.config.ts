67195af78f1fed52ca5490e04c59250283378a63
Mateusz <mateuszsawicki1509@gmail.com>
2024-01-29 19:43:04 +0100

    zbyudowanie public

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '/biuro-kip/',
   build: {
     outDir: 'public',
   },
