15794c33315145c359fa97324fa74bcfb1a7bd18
Mateusz <mateuszsawicki1509@gmail.com>
2024-02-05 17:40:15 +0100

    usuniecie nieuzywanych zdjec

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/biuro-kip/',
-  build: {
-    outDir: 'public',
-  },
+  // base: '/biuro-kip/',
 })
