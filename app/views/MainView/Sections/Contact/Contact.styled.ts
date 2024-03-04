27ee28a2dfbb50a2de135d2cd07287d0f067c82e
Mateusz <mateuszsawicki1509@gmail.com>
2024-03-04 22:35:03 +0100

    poprawki po testach

--- app/views/MainView/Sections/Contact/Contact.styled.ts
+++ app/views/MainView/Sections/Contact/Contact.styled.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const StyledContent = styled.div`
   width: 100%;
   height: 100%;
-  padding-top: 80px;
+  padding-top: 60px;
   display: flex;
   align-items: center;
   justify-content: end;
